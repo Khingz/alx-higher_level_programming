e26fc61e730af3bb71caf181f40bbbd6fbb81d7e
Khingz <kingsley995mr@gmail.com>
2023-06-06 04:49:57 -0700

    improve cases of number being negative and printing a negative ast digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 num = abs(number) % 10
+if number < 0:
+    num = num * -1
 if num > 5:
     print(f"Last digit of {number:d} is {num:d} and is greater than 5")
 elif num == 0:
