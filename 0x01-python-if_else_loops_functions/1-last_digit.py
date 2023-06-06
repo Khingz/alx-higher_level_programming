2f0f4f027832faf87365868d0de4ee78dafffc53
Khingz <kingsley995mr@gmail.com>
2023-06-06 04:20:49 -0700

    refactored task 1 to include else keyword

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ if number < 0:
 else:
     num = number % 10
 
-if num < 6 and num != 0:
-    print(f"Last digit of {number:d} is {num:d} and is less than 6 and not 0")
-elif num > 5:
+if num > 5:
     print(f"Last digit of {number:d} is {num:d} and is greater than 5")
 elif num == 0:
     print(f"Last digit of {number:d} is {num:d} and is 0")
+else:
+    print(f"Last digit of {number:d} is {num:d} and is less than 6 and not 0")
