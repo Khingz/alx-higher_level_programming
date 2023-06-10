7f07930edfc69f196d591525dbbd5b256938c478
Khingz <kingsley995mr@gmail.com>
2023-06-10 00:07:07 -0700

    set initial max_num to be first element in list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
     if isinstance(my_list, list):
         if len(my_list) == 0:
             return None
-        max_n = 0
+        max_n = my_list[0]
         for n in my_list:
             if n > max_n:
                 max_n = n
