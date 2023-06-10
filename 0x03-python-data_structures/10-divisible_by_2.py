78c1e7ae95c395ee3d6963e415e3d4906bef94c4
Khingz <kingsley995mr@gmail.com>
2023-06-09 23:56:20 -0700

    checked if arg is a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    mul_2 = []
-    for n in my_list:
-        if n % 2 == 0:
-            mul_2.append(True)
-        else:
-            mul_2.append(False)
-    return mul_2
+    if isinstance(my_list, list):
+        mul_2 = []
+        for n in my_list:
+            if n % 2 == 0:
+                mul_2.append(True)
+            else:
+                mul_2.append(False)
+        return mul_2
