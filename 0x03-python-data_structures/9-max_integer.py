d7d84f66eda430680a7cf770d15222ddfcd4a45b
Khingz <kingsley995mr@gmail.com>
2023-06-09 23:59:08 -0700

    edge case for arg that are not list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    max_n = 0
-    for n in my_list:
-        if n > max_n:
-            max_n = n
-    return max_n
+    if isinstance(my_list, list):
+        if len(my_list) == 0:
+            return None
+        max_n = 0
+        for n in my_list:
+            if n > max_n:
+                max_n = n
+        return max_n
