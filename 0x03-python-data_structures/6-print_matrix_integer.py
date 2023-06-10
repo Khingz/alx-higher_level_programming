48b1653b87a3013f9280149d81d6daae04a001c6
Khingz <kingsley995mr@gmail.com>
2023-06-09 22:39:25 -0700

    included format specifier

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for n in matrix:
         for i in range(len(n)):
             if i != (len(n) - 1):
-                print("{} ".format(n[i]), end="")
+                print("{:d} ".format(n[i]), end="")
             else:
                 print(n[i], end="")
         print()
