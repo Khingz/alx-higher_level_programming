b1a2705147deb52a7938b7c7a6cac9001f2310ee
Khingz <kingsley995mr@gmail.com>
2023-06-14 04:49:45 -0700

    remoced if check

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    if isinstance(my_list, list) and search and replace:
-        return [(item if item != search else replace) for item in my_list]
+    return [(item if item != search else replace) for item in my_list]
