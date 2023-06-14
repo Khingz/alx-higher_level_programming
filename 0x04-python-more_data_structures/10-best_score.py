8e9d506be4bbab5aaf20aa8bdd06844c684b7d5f
Khingz <kingsley995mr@gmail.com>
2023-06-14 08:28:29 -0700

    refactor code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,10 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    return max(a_dictionary, key=a_dictionary.get)
+    user = list(a_dictionary.keys())[0]
+    max_n = a_dictionary[user]
+    for key, val in a_dictionary.items():
+        if val > max_n:
+            big = val
+            user = key
+    return (user)
