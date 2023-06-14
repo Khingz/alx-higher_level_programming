4d992202859ebd2dbe837507a4a8a0d3217808ce
Khingz <kingsley995mr@gmail.com>
2023-06-14 11:33:16 -0700

    fix: code style and guard for empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    deno = 0;
-    numer = 0;
-    for  num in my_list:
-        a, b = num
-        deno += b
-        numer += (a * b)
-    return (numer / deno)
+    if isinstance(my_list, list) and len(my_list) != 0:
+        deno = 0
+        numer = 0
+        for num in my_list:
+            a, b = num
+            deno += b
+            numer += (a * b)
+        return (numer / deno)
