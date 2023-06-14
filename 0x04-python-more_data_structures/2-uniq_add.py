d5ed1cc8e322ca9643f5973f39024cec8b250e08
Khingz <kingsley995mr@gmail.com>
2023-06-14 05:11:43 -0700

    used sum function

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    total = None
-    for i in list(set(my_list)):
-        total = (i if total is None else total + i)
-    return total
+    return sum(set(my_list))
