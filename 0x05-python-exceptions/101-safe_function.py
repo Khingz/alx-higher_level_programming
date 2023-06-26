c67c40d9e677034b720c6057b2c30c8fcb230a82
Khingz <kingsley995mr@gmail.com>
2023-06-26 05:33:31 -0700

    retunrd from the function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,6 +4,7 @@ from sys import stderr
 
 def safe_function(fct, *args):
     try:
-        fct(*args)
+        return fct(*args)
     except Exception as e:
         print("Exception:", str(e), file=stderr)
+        return None
