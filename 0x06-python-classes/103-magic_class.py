5a1cd66dd93c94ccdf01dd1b53fcce50dd5c7f5f
Khingz <kingsley995mr@gmail.com>
2023-06-27 22:16:51 -0700

    coding style

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 
 class MagicClass:
-    def __init__(self, radius=0):
-        """initialize variblaes"""
+    """Initialize a MagicClass for cirile rasius"""
+
+    def __init__(self, radius):
+        """initialize variblaes
+        Args:
+            radius: the radius
+
+        """
+        self.__radius = 0
         if type(radius) is not int or type(radius) is not float:
             raise TypeError("radius must be a number")
-         self.__radius = radius
+        self.__radius = radius
 
-         def area(self):
-             """Return the area of the MagicClass."""
-             return (self.__radius ** 2 * math.pi)
+        def area(self):
+            """Return the area of the MagicClass."""
+            return (self.__radius ** 2 * math.pi)
 
-         def circumference(self):
-             """Return The circumference of the MagicClass."""
-             return (2 * math.pi * self.__radius)
+        def circumference(self):
+            """Return The circumference of the MagicClass."""
+            return (2 * math.pi * self.__radius)
