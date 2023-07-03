21bd203f180b8046bcb454efeb8b6fda6fdb79ef
Khingz <kingsley995mr@gmail.com>
2023-07-03 04:33:53 -0700

    used if statement instead of elif

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
         """sets width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -39,6 +39,6 @@ class Rectangle:
         """sets height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
