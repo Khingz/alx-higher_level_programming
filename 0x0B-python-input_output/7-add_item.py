59aad583251ac8ec94160f3e5a56e1d6ef0b68e8
Khingz <kingsley995mr@gmail.com>
2023-07-11 04:12:10 -0700

    refactored code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,15 +6,13 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def main():
-    with open("add_item.json", 'w') as fw:
-        with open("add_item.json", 'r') as fr:
-            if fr.read() == '':
-                f_r = []
-            else:
-                f_r = load_from_json_file("add_item.json")
-            for arg in sys.argv[1:]:
-                f_r.append(arg)
-            save_to_json_file(f_r, "add_item.json")
+    try:
+        f_r = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        f_r = []
+    for arg in sys.argv[1:]:
+        f_r.append(arg)
+    save_to_json_file(f_r, "add_item.json")
 
 
 if __name__ == "__main__":
