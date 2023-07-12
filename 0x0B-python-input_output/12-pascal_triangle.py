7cbd5955396717e42d90a229f160a2a71e9e227e
Khingz <kingsley995mr@gmail.com>
2023-07-12 13:16:42 -0700

    added [ascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A module that defined a pascal triangle function"""
+
+
+def pascal_triangle(n):
+    """A function that returns a pascal tringle"""
+    triangle = [[1]]
+    for i in range(1, n):
+        zero_triangle = [0] + triangle[i - 1] + [0]
+        elem = []
+        for j in range(len(zero_triangle) - 1):
+            elem.append(zero_triangle[j] + zero_triangle[j + 1])
+        triangle.append(elem)
+    return triangle
