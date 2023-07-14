05c66d13c6587aac5e1d4d20f4c1d3a34f5598c9
Khingz <kingsley995mr@gmail.com>
2023-07-13 23:28:42 -0700

    added task 11 and forst push

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,20 @@ class Square(Rectangle):
         """init class"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """gets the value of size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise TypeError("width must be > 0")
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """print class to stdout"""
         return "[Square] ({}) {}/{} - {}".format(
