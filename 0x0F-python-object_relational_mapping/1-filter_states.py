7d2417d7e0c76a545592ea7e2be877b63503aaf0
Khingz <kingsley995mr@gmail.com>
2023-09-15 01:39:29 -0700

    check 5

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__" and len(sys.argv) == 4:
             db=sys.argv[3])
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE\
-            'N%' ORDER BY states.id")
+            'N%' ORDER BY id")
     rows = cursor.fetchall()
     for i in rows:
         print(i)
