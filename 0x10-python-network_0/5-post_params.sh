f4fde98ec9b1f0788f8836c207310fad4560cd95
Khingz <kingsley995mr@gmail.com>
2023-09-28 05:08:35 -0700

    silenece

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post request with curl
-curl -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
+curl -d "email=test@gmail.com&subject=I will always be here for PLD" -sX POST "$1"
