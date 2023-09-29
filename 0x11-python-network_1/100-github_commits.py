45c14633df64ae1dea0416d9f6e0251d6b854da7
Khingz <kingsley995mr@gmail.com>
2023-09-29 11:19:06 -0700

    modified code to us get method

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     user = sys.argv[2]
     repo = sys.argv[1]
 
-    payload = {'committer': 'rails', 'per_page': 10}
+    payload = {'committer': 'rails'}
 
     url = f'https://api.github.com/repos/{user}/{repo}/commits'
     res = requests.get(url)
@@ -17,7 +17,7 @@ if __name__ == "__main__":
             res = res.json()
             for i in range(10):
                 print("{}: {}".format(
-                    res[i]['sha'],
-                    res[i]['commit']['author']['name']))
+                    res[i].get('sha'),
+                    res[i]i.get('commit').get('author').get('name')))
         except ValueError:
             pass
